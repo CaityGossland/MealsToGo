d2dbb05796d55d9b52918079c020368b13752e6f
Caity Gossland <caitygossland@gmail.com>
2021-10-07 16:37:50 -0700

    Sets up working basic app, adds react-native-paper, styled-components, eslint/prettier etc

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card.Cover)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.colors.ui.primary};
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 Random Street, Victoria, BC",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <RestaurantCard>
+      <RestaurantCardCover source={{ uri: photos[0] }} />
+      <Title>{name}</Title>
+    </RestaurantCard>
+  );
+};
